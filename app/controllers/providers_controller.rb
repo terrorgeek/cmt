830e25b8910b4380f820a74a8249285bc87693f7
painforever <bjslxxx@126.com>
2022-04-15 23:37:40 -0400

    server side validation

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -5,6 +5,12 @@ class ProvidersController < ApplicationController
 
   def create
     provider_from_api = Provider.query(params[:npi])
+
+    unless validate_npi
+      flash[:error] = "NPI is invalid!"
+      redirect_back fallback_location: nil and return
+    end
+
     if provider_from_api.has_key?(:error)
       flash[:error] = provider_from_api[:error]
       redirect_back fallback_location: nil and return
@@ -21,5 +27,10 @@ class ProvidersController < ApplicationController
       flash[:create_success] = "Create Success!"
       redirect_back fallback_location: nil and return
     end
+  end
+
+  private 
+  def validate_npi
+    return params[:npi] =~ /^\d{10,11}$/
   end
 end
